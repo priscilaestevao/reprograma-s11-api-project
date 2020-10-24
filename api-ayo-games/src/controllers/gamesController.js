b9db305d14eb6cf5e3107d6eb8b4eec20d1e4751
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-10-23 23:41:14 -0300

    get game by id

--- api-ayo-games/src/controllers/gamesController.js
+++ api-ayo-games/src/controllers/gamesController.js
@@ -1,10 +1,21 @@
 const games = require("../models/games.json");
 
 const getAllGames = (req, res) => {
-  console.log(req.url);
   res.status(200).send(games);
 };
 
+const getGameById = (req, res) => {
+  const gameId = req.params.id;
+  const gameFound = games.find(game => game.id == gameId);
+
+  if (gameFound) {
+    res.status(200).send(gameFound);
+  } else {
+    res.status(404).send({ message: "Game not found" });
+  }
+};
+
 module.exports = {
-    getAllGames
+  getAllGames,
+  getGameById,
 };
--- api-ayo-games/src/routes/gamesRoute.js
+++ api-ayo-games/src/routes/gamesRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require("../controllers/gamesController");
 
 router.get("/", controller.getAllGames);
+router.get("/:id", controller.getGameById);
 
 module.exports = router;
