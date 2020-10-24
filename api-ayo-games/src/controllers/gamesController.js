5e87361fd28234cc3463602a325fbfe20643d120
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-10-24 02:28:35 -0300

    put game

--- api-ayo-games/src/controllers/gamesController.js
+++ api-ayo-games/src/controllers/gamesController.js
@@ -32,7 +32,7 @@ const registerGame = (req, res) => {
       "utf8",
       (err) => {
         if (err) {
-          return res.status(424).send({ message: err });
+          return res.status(424).send({ message: "Error adding game" });
         }
         console.log("Game successfully added!");
       }
@@ -43,8 +43,36 @@ const registerGame = (req, res) => {
   }
 };
 
+const updateGame = (req, res) => {
+  try {
+    const gameId = req.params.id;
+    const newInfomations = req.body;
+    const gameFound = games.find((game) => game.id == gameId);
+    const gameIndex = games.indexOf(gameFound);
+
+    if (gameIndex >=0 ) {
+        games.splice(gameIndex, 1, newInfomations)
+    } else {
+        res.status(404).send({ message: "Game not found to be updated"})
+    }
+
+    fs.writeFile("./src/models/games.json", JSON.stringify(games), "utf8", (err) => {
+        if (err) {
+            return res.status(424).send({ message: "Error updating game" })
+        }
+        console.log("Game updated successfully!")
+        const gameUpdated = games.find((game) => game.id == gameId)
+        res.status(200).send(gameUpdated);
+    })
+
+  } catch {
+    res.status(500).send({ message: "Internal server error" });
+  }
+};
+
 module.exports = {
   getAllGames,
   getGameById,
   registerGame,
+  updateGame,
 };
--- api-ayo-games/src/routes/gamesRoute.js
+++ api-ayo-games/src/routes/gamesRoute.js
@@ -5,5 +5,6 @@ const controller = require("../controllers/gamesController");
 router.get("/", controller.getAllGames);
 router.get("/:id", controller.getGameById);
 router.post("/", controller.registerGame);
+router.put("/:id", controller.updateGame);
 
 module.exports = router;
