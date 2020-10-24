7080cf77bed3b612620f723996d11bbdf965f336
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-10-23 22:28:18 -0300

    server created

---
+++ api-ayo-games/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+module.exports = app;
